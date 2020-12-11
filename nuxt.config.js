6bde5677ebd61ac81a46a718e1c230f836045c10
AbonyiXavier <francisabonyi@gmail.com>
2020-12-11 22:07:13 +0100

    added procfile file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,8 @@
-import colors from "vuetify/es5/util/colors";
+// import colors from "vuetify/es5/util/colors";
 
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
-  ssr: true,
+
   head: {
     titleTemplate: "%s - e-commerce",
     title: "e-commerce",
@@ -36,18 +36,18 @@ export default {
   vuetify: {
     customVariables: ["~/assets/variables.scss"],
     theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
+      dark: true
+      // themes: {
+      //   dark: {
+      //     primary: colors.blue.darken2,
+      //     accent: colors.grey.darken3,
+      //     secondary: colors.amber.darken3,
+      //     info: colors.teal.lighten1,
+      //     warning: colors.amber.base,
+      //     error: colors.deepOrange.accent4,
+      //     success: colors.green.accent3
+      //   }
+      // }
     }
   },
 
